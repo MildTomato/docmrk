964931a58f9a3a54fed250ccc9ef3b06f80c6ad2
Jonathan Summers-Muir <MildTomato@users.noreply.github.com>
2023-10-27 14:13:02 +0800

    move extension

--- apps/extension/popup.tsx
+++ apps/extension/popup.tsx
@@ -6,17 +6,19 @@ function IndexPopup() {
   return (
     <div
       style={{
+        width: "320px",
         display: "flex",
         flexDirection: "column",
         padding: 16
       }}>
       <h2>
-        Welcome to your{" "}
+        JONNY Welcome to your{" "}
         <a href="https://www.plasmo.com" target="_blank">
           Plasmo
         </a>{" "}
         Extension!
       </h2>
+      {data}
       <input onChange={(e) => setData(e.target.value)} value={data} />
       <a href="https://docs.plasmo.com" target="_blank">
         View Docs
